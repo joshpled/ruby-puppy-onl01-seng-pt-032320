b0938040d8e2215098099e4fc6b7d67b31994c6a
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-14 02:28:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,11 @@ class Dog
 
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     save
+=======
+    @@all << self
+>>>>>>> 2c38153140322d8c900acfa0020f826e68377bc7
   end
   def self.all
     @@all
@@ -18,9 +22,16 @@ class Dog
   def self.print_all
     @@all.each do |n|
       puts n.name
+<<<<<<< HEAD
     end
   end
   def save
     @@all << self
   end
 end
+=======
+     end
+  end
+end
+binding.pry
+>>>>>>> 2c38153140322d8c900acfa0020f826e68377bc7
